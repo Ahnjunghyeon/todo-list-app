1d5e294b1f3bc34cb6fb51f1c910edf171cb96dc
Ahnjunghyeon <1271wndgusdl@naver.com>
2024-01-29 18:43:40 +0900

    [1/n] Create TodoItemInputField

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
+const TodoItemInputField = (props) => {
+  return <div></div>;
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoItemInputField />
+    </div>
+  );
 }
 
 export default App;
