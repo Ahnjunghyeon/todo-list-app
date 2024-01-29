10e7d7e6dab9148ed7c12d46968d62edb13c2bf3
Ahnjunghyeon <1271wndgusdl@naver.com>
2024-01-29 20:06:03 +0900

    [3/n] Submit button +

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import "./App.css";
 import { useState } from "react";
 
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
+  const onSubmit = () => {
+    // onSubmit =폼이 제출될 때 무언가를 수행
+    // props.onSubmit => TodoItemInputField 컴포넌트를 사용하는 ~에게서 onSubmit을 받아오는 코드
+    // 즉 App의       <TodoItemInputField /> 에서 받아옴
+    props.onSubmit(input);
+    setInput("");
+  };
+
   return (
     <div>
       <TextField
@@ -14,6 +23,9 @@ const TodoItemInputField = (props) => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
+      <Button variant="outlined" onClick={onSubmit}>
+        Submit
+      </Button>
     </div>
   );
 };
@@ -21,7 +33,7 @@ const TodoItemInputField = (props) => {
 function App() {
   return (
     <div className="App">
-      <TodoItemInputField />
+      <TodoItemInputField onSubmit={() => {}} />
     </div>
   );
 }
