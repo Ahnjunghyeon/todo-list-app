4e00eb5d5ae728c6fe3c8fb3664ed36c7ee1d3d4
Ahnjunghyeon <1271wndgusdl@naver.com>
2024-01-30 17:56:51 +0900

    1/30 start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+// TodoItemInputField의 하는일 => 입력 + 제출 버튼
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
   const onSubmit = () => {
@@ -31,8 +32,9 @@ const TodoItemInputField = (props) => {
 };
 
 // 현재까지 추가된 TodoItem리스트를 받아오기 위해서 Props를 이용해서 다른 컴포넌트에서 받아와야 함.
+// TodoItemList의 하는일 => 제출된 아이템 리스트를 Array로 만들어 제공
 const TodoItemList = (props) => {
-  // map 새로운 배열을 받아오는 데 사용
+  // TodoItemList 의 todoItem를 Array로 받아와서 map 을통해 리스트아이템으로 반환
   const todoList = props.TodoItemList.map((todoItem, index) => {
     return <li key={index}>{todoItem.todoItemCOntent}</li>;
   });
@@ -40,6 +42,8 @@ const TodoItemList = (props) => {
 };
 
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+
   return (
     <div className="App">
       <TodoItemInputField
@@ -48,7 +52,7 @@ function App() {
         }}
       />
       {/* 해당부분부터 다시하기 */}
-      <TodoItemList todoItemList={[{}]} />
+      <TodoItemList todoItemList={[]} />
     </div>
   );
 }
